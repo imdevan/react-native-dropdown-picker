a0391722e7d2e01240fd9f8f2656bef50497f986
imdevan <huapayadevan@gmail.com>
2025-10-11 23:23:59 -0700

    update metro.config to ignore parent node_modules

--- examples/metro.config.js
+++ examples/metro.config.js
@@ -2,8 +2,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 // const exclusionList = require('metro-config/src/defaults/exclusionList');
 const path = require('path');
 
+// Helper to safely embed a path in RegExp
+const escapeRegExp = (s) => s.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(__dirname, '..');
+const parentNodeModules = path.join(workspaceRoot, 'node_modules');
 
 const config = getDefaultConfig(projectRoot);
 
@@ -13,11 +17,12 @@ config.resolver = {
   ...config.resolver,
   nodeModulesPaths: [
     path.join(projectRoot, 'node_modules'),
-    path.join(workspaceRoot, 'node_modules'),
   ],
   
   // Ignore common explosion points
   blockList: [
+    new RegExp(`^${escapeRegExp(parentNodeModules)}[/\\\\].*`),
+
     // avoid triply nested node_modules to resolve issue with using a local copy of the module
     /.*\/node_modules\/.*\/node_modules\/.*\/node_modules\/.*/,
 
