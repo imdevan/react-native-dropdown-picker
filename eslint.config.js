f24a1375d8212fa6195c51bb2b6c7dd317acc449
imdevan <huapayadevan@gmail.com>
2025-10-11 23:32:29 -0700

    ignore no-unresolved for eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import eslint from '@eslint/js';
+// eslint-disable-next-line import/no-unresolved
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
